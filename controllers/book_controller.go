cd49971e5addad36ab0cf5537d5ebda3cd92b9d9
Heberth Junor <hjunor@gmail.com>
2021-11-03 12:27:21 -0300

    add: adeed method in update and delete books

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -78,3 +78,59 @@ func ShowBooks(c *gin.Context) {
 
 	c.JSON(200, books)
 }
+
+func UpdateBook(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var book models.Book
+
+	err := c.ShouldBindJSON(&book)
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot bind json" + err.Error(),
+		})
+		return
+	}
+
+	err = db.Save(&book).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot create book" + err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, book)
+
+}
+
+func DeleteBook(c *gin.Context) {
+	db := database.GetDatabase()
+	id := c.Param("id")
+
+	newid, err := strconv.Atoi(id)
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "ID has to be interger",
+		})
+		return
+	}
+
+	var book models.Book
+
+	err = db.Delete(&book, newid).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": " cannot find book:  " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, book)
+
+}
